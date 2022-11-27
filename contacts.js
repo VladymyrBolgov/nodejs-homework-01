e24e084356b153949392dbbc20f5685d86ba38c3
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-27 19:35:10 +0200

    fh

--- contacts.js
+++ contacts.js
@@ -5,7 +5,7 @@ const {v4} = require('uuid')
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
 const listContacts = async () => {
-
+    const data = 
 }
 
 const getContactById = async contactId => {
@@ -20,3 +20,10 @@ const addContact = async (name, email, phone) => {
     
 }
 
+const contactsAction = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
+module.exports = contactsAction;
